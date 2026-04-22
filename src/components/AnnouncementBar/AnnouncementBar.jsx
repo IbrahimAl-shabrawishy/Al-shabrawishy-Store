4923beeb80eb32b0dc51099813d2fb5c03a52887
Ibrahim Al-shabrawishy <ibrahimelshabrawishy123321@gmail.com>
2026-04-22 10:39:00 +0200

    install tailwind css, react icons and react router dom

---
+++ src/components/AnnouncementBar/AnnouncementBar.jsx
@@ -0,0 +1,10 @@
+// import { Link } from "react-router-dom";
+export default function AnnouncementBar() {
+  return (
+    <section>
+      <p className="text-[#FFEBFC] p-2 text-center text-[14px]  bg-[#3B82F6]">
+        ✨ 30% off your first order – Shop now!
+      </p>
+    </section>
+  );
+}
